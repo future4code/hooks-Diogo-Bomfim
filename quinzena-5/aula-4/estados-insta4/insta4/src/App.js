fbc055784259adf7af92cf4dee2ec574715aeb35
dev-Diogo <diogohbomfim@gmail.com>
2022-04-28 22:42:55 -0300

    Adicionei a atividade de react ao arquivo (incompleto)

--- quinzena-5/aula-4/estados-insta4/insta4/src/App.js
+++ quinzena-5/aula-4/estados-insta4/insta4/src/App.js
@@ -14,28 +14,15 @@ class App extends React.Component {
     return (
       <MainContainer>
         <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
+
+          
+
         />
         
-        <MainContainer>
-          <Post
-            nomeUsuario={"Diogo"}
-            fotoUsuario={'https://picsum.photos/50/51'}
-            fotoPost={'https://picsum.photos/200/152'}
-          />
-        </MainContainer>
-
-        <MainContainer>
-          <Post
-            nomeUsuario={"Pedro"}
-            fotoUsuario={'https://picsum.photos/50/52'}
-            fotoPost={'https://picsum.photos/200/151'}
-          />
-        </MainContainer>
         
       </MainContainer>
+         
+        
     
     );
   }

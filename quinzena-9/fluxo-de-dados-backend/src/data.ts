b1a692cee5cf3ce198c82d19d63a24d81098f86b
DiogoB0mfim <diogohbomfim@gmail.com>
2022-08-23 19:53:01 -0300

    atividade fluxo de dados backend

---
+++ quinzena-9/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,16 @@
+type TipoProduto = {id: string, name: string, price: number}
+export const produtos: TipoProduto[] = [{
+    id: "1",
+    name: "Batata",
+    price: 15
+},
+{
+    id: "2",
+    name: "Pão",
+    price: 5   
+},
+{
+    id: "3",
+    name: "Mamão",
+    price: 7   
+}]

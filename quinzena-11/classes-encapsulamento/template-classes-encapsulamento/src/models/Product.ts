59d734a28f4769400ec398045f0da638c3c20e3d
DiogoB0mfim <diogohbomfim@gmail.com>
2022-09-26 23:31:40 -0300

    atividade de classes e encapsulamento finalizadas

---
+++ quinzena-11/classes-encapsulamento/template-classes-encapsulamento/src/models/Product.ts
@@ -0,0 +1,13 @@
+// export type Product = {id: string, name: string, price: number }
+
+export class Product {
+    constructor( 
+        public id: string,
+        public name: string, 
+        public price: number
+    ){
+        this.id = id
+        this.name = name
+        this.price = price
+    }
+}
